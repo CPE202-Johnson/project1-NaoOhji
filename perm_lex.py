d5b2a787cde48d8eba2d003638c6eeef9d29cf9c
lab0-NaoOhji <nmuroga@calpoly.edu>
2021-01-18 19:48:49 -0800

    third try

--- perm_lex.py
+++ perm_lex.py
@@ -1,3 +1,16 @@
-def perm_gen_lex(a): 
+def perm_gen_lex(a):
+    
+    if  len(a) <= 0:
+        return ''
+    elif len(a)==1:
+        return a
+    
+    newList = []
+    for x in range(len(a)):
+        temp = list(a)
+        temp.pop(x)
+        newList.append(str(a[x])+perm_gen_lex("".join(temp)))
+    return newList
 
  
+
