e967d8f426c7503b313696c1d7be36a4f1e5f58f
lab0-NaoOhji <nmuroga@calpoly.edu>
2021-01-18 17:32:51 -0800

    second try

--- bears.py
+++ bears.py
@@ -1,1 +1,19 @@
 def bears(n):
+    if n==42:
+        return True
+    if n%2==0:
+        return bears(n/2)
+    elif n%3==0 or n%4==0:
+        t1 = n
+        t1 /= 100
+        t1 *= 100
+        t1 = (n-t1)/10
+        t2 = n
+        t2 /= 10
+        t2 *= 10
+        t2 = n-t2
+        return bears(t1*t2)
+    elif n%5==0:
+        return bears(42)
+    return False
+        
