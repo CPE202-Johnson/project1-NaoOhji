5474bccfe7b0a6d874a5dbb4d89a9a0bc2b0e8b4
lab0-NaoOhji <nmuroga@calpoly.edu>
2021-01-18 17:22:30 -0800

    first try

--- base_convert.py
+++ base_convert.py
@@ -1,3 +1,10 @@
 
 def convert(num, b):
-    """Recursive function that returns a string representing num in the base b"""
+    numStrArray = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
+    if num/b == 0:
+        if num%b!=0:
+            return numStrArray[num%b]
+        else:
+            return ""
+    else:
+        return convert(num/b,b)+numStrArray[num%b]
